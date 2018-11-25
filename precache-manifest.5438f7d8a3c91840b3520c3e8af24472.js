2a6f89d0d888c320b2b8f16160c1f8454d13d196
shamilkhandemo <41108271+shamilkhandemo@users.noreply.github.com>
2018-11-25 14:29:45 +0300

    Add files via upload

---
+++ precache-manifest.5438f7d8a3c91840b3520c3e8af24472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e77607dadfee271483d9",
+    "url": "/static/js/main.e77607da.chunk.js"
+  },
+  {
+    "revision": "7e3788b5b51c0e691302",
+    "url": "/static/js/1.7e3788b5.chunk.js"
+  },
+  {
+    "revision": "e77607dadfee271483d9",
+    "url": "/static/css/main.99ed85d3.chunk.css"
+  },
+  {
+    "revision": "7e3788b5b51c0e691302",
+    "url": "/static/css/1.cf7613a4.chunk.css"
+  },
+  {
+    "revision": "0ebb3ff9234f3491808d76650d189672",
+    "url": "/index.html"
+  }
+];
